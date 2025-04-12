060c3215455d392410eb890cce419b7deadc6f1d
Igor Rivin <rivin@temple.edu>
2025-04-12 19:04:42 -0400

    Add function aliases in __init__.py to fix pylint warnings about undefined names

--- graphem/__init__.py
+++ graphem/__init__.py
@@ -24,4 +24,16 @@ from graphem.visualization import (
     display_benchmark_results
 )
 
+# Add aliases for backward compatibility and to fix pylint warnings
+generate_erdos_renyi_graph = erdos_renyi_graph
+generate_barabasi_albert_graph = generate_ba
+generate_watts_strogatz_graph = generate_ws
+generate_random_regular_graph = generate_random_regular
+generate_sbm_graph = generate_sbm
+generate_scale_free_graph = generate_scale_free
+generate_geometric_graph = generate_geometric
+generate_caveman_graph = generate_caveman
+generate_relaxed_caveman_graph = generate_relaxed_caveman
+visualize_graph = plot_radial_vs_centrality  # This was likely the intended alias
+
 __version__ = '0.1.0'
