9772533f21acb87caae82cd71f731a1317f486d5
Igor Rivin <rivin@temple.edu>
2025-04-12 18:53:16 -0400

    Fix module imports in __init__.py to match actual function names

--- graphem/__init__.py
+++ graphem/__init__.py
@@ -7,15 +7,15 @@ from graphem.index import HPIndex
 from graphem.influence import graphem_seed_selection, ndlib_estimated_influence, greedy_seed_selection
 from graphem.datasets import load_dataset
 from graphem.generators import (
-    generate_erdos_renyi_graph,
-    generate_barabasi_albert_graph,
-    generate_watts_strogatz_graph,
-    generate_random_regular_graph,
-    generate_sbm_graph,
-    generate_scale_free_graph,
-    generate_geometric_graph,
-    generate_caveman_graph,
-    generate_relaxed_caveman_graph
+    erdos_renyi_graph,
+    generate_ba,
+    generate_ws,
+    generate_random_regular,
+    generate_sbm,
+    generate_scale_free,
+    generate_geometric,
+    generate_caveman,
+    generate_relaxed_caveman
 )
 from graphem.visualization import visualize_graph, plot_benchmark_results
 
