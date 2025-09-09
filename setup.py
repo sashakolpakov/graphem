55ad2c05446e2c9d1c99f663c6f43e6c1f689e98
Sasha Kolpakov <kolpakov.alexander@gmail.com>
2025-09-09 12:12:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -37,6 +37,10 @@ docs_required = [
     "sphinx-autodoc-typehints>=1.12.0"
 ]
 
+test_required = [
+    "pytest>=6.0.0",
+]
+
 setup(
     name="graphem-jax",
     version="0.2.0",
@@ -48,6 +52,7 @@ setup(
     install_requires=required,
     extras_require={
         "docs": docs_required,
+        "test": test_required,
     },
     python_requires=">=3.8",
     classifiers=[
